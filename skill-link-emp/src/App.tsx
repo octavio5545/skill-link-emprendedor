820706089d51454e37e8816985ccf4b926b68ff7
ïng.jmcarmona@gmail.com <pe@MacBook-Air-de-Jose.local>
2025-06-20 13:27:22 -0600

    Agrega componente NavBarWrapper

--- skill-link-emp/src/App.tsx
+++ skill-link-emp/src/App.tsx
@@ -3,13 +3,15 @@ import { EntrepreneurDashboard } from './components/EntrepreneurDashboard';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Login } from './components/Login/Login';
 import { ResetPasswordPage } from './components/Login/pages/ResetPasswordPage.tsx'
+import NavBarWrapper from './components/NavBar/NavBarWrapper';
 
 import './App.css'
 
 function App() {
 
   return (
-    <Router>      
+    <Router>
+      <NavBarWrapper />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
